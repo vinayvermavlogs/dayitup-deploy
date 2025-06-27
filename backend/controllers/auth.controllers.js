4c794647c77e6b0961e6e879343e72285502d727
Vinay <149930169+vinayvermavlogs@users.noreply.github.com>
2025-06-27 23:37:02 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -36,8 +36,8 @@ const token=await genToken(user._id)
 res.cookie("token",token,{
     httpOnly:true,
     maxAge:7*24*60*60*1000,
-    sameSite:"Strict",
-    secure:false
+    sameSite:"None",
+    secure:true
    })
 
    return res.status(201).json(user)
@@ -68,8 +68,8 @@ export const login=async (req,res)=>{
  res.cookie("token",token,{
      httpOnly:true,
      maxAge:7*24*60*60*1000, // milisecond
-     sameSite:"Strict",
-     secure:false
+     sameSite:"None",
+     secure:true
     })
  
     return res.status(200).json(user)
